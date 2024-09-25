059d5c6348778c5612f25020d2f854f17a80abba
RamonDonadeu <ramondonadeucaballero@gmail.com>
2024-09-25 20:14:50 +0200

    feat: Remake Database

--- prisma/migrations/20240817123109_/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "player_league_participation" ADD COLUMN     "draws" INTEGER NOT NULL DEFAULT 0,
-ADD COLUMN     "losses" INTEGER NOT NULL DEFAULT 0,
-ADD COLUMN     "wins" INTEGER NOT NULL DEFAULT 0;
